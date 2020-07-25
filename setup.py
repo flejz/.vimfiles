0cf4a52475fc3a0d257c8d31ce3c9909cb82e363
Jaime Flores <jaimelopesflores@gmail.com>
2020-07-26 00:48:55 +0200

    sending the output from git clone to hell

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/flejz/.vim.git ~/.vim
+git clone https://github.com/flejz/.vim.git ~/.vim >/dev/null
 echo "source ~/.vim/vimrc" > ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle >/dev/null
 vim +BundleInstall +qall

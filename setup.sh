efba1e332da17c66bcaab7cf5caedbac771ef941
Jaime Flores <jaimelopesflores@gmail.com>
2020-07-26 02:14:54 +0200

    add quiet flag in the git clones

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/flejz/.vim.git ~/.vim >/dev/null
+git clone --quiet https://github.com/flejz/.vim.git ~/.vim >/dev/null
 echo "source ~/.vim/vimrc" > ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle >/dev/null
+git clone --quiet https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle >/dev/null
 vim +BundleInstall +qall
 
-git clone  https://github.com/powerline/fonts .~/.temp-fonts >/dev/null
+git clone --quiet https://github.com/powerline/fonts .~/.temp-fonts >/dev/null
 sh ~/.temp-fonts/install.sh
 rm -rf ~/.temp-fonts

9ac3c216e08acaf073ac75753e419c261a9331ce
Jaime Flores <jaimelopesflores@gmail.com>
2021-08-08 17:29:45 +0200

    replace ale with coc

--- setup.sh
+++ setup.sh
@@ -7,13 +7,13 @@ clone() {
   [[ $REPO =~ \/(.*)$ ]]
   SHORT=${BASH_REMATCH[1]}
   echo "clonning $REPO"
-  git clone --depth=1 --quiet https://github.com/$REPO.git ~/.vim/pack/plugins/start/$SHORT 2>/dev/null
+  git clone --depth=1 --quiet "https://github.com/$REPO.git" "$HOME/.vim/pack/plugins/start/$SHORT" 2>/dev/null
 }
 
 PLUGIN_LIST=(
   "kien/ctrlp.vim"
   "vim-airline/vim-airline"
-  "dense-analysis/ale"
+  "neoclide/coc.nvim"
 )
 
 LANG_LIST=(
@@ -22,14 +22,15 @@ LANG_LIST=(
 )
 
 for i in ${PLUGIN_LIST[*]}; do
-  clone $i
+  clone "$i"
 done
 
 for i in ${LANG_LIST[*]}; do
-  clone $i
+  clone "$i"
 done
 
 ln -s ~/.vim/vimrc ~/.vimrc
 echo "install https://github.com/golang/tools/blob/master/gopls for go"
 echo "install https://github.com/MaskRay/ccls for cpp"
 echo "install https://github.com/Microsoft/TypeScript/wiki/Standalone-Server-%28tsserver%29 for typescript"
+echo "install https://github.com/koalaman/shellcheck for shell script"
